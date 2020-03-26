a7906f29ffc50617be7166195d47fbc56398cf16
nicos <nicolas.turck@gmail.com>
2020-03-26 09:12:23 +0100

    add request for create new questions

--- WorldTransit/blog/forms.py
+++ WorldTransit/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Users, Response
+from .models import Users, Response, Questions
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -9,25 +9,13 @@ class SignUpForm(forms.ModelForm):
         model = Users
         fields = '__all__'
 
-"""class SignUpForm(forms.Form):
-    name = forms.CharField(max_length=255, required=False)
-    last_name = forms.CharField(max_length=255, required=False)
-    pseudo = forms.CharField(max_length=255, required=False)
-    job = forms.CharField(max_length=255, required=False)
-    password = forms.CharField(max_length=255, required=False)"""
-
-    #class Meta:
-       # model = User
-
-
 class ResponseForm(forms.ModelForm):
     class Meta:
         model = Response
         fields = '__all__'
-    """title = forms.CharField(label='title', max_length=255, required=False)
-    content = forms.CharField(label='reponse', required=False)
-    publishing_date = forms.DateTimeField(label='date',required=False)
-    updated_date = forms.DateTimeField(label='update',required=False)
-    author = forms.CharField(label='auteur', max_length=255, required=False)
-    #user = forms.ForeignKey(Users, on_delete=forms.CASCADE, required=False)"""
 
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Questions
+        fields = '__all__'
--- WorldTransit/blog/views.py
+++ WorldTransit/blog/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import SuccessURLAllowedHostsMixin, FormView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, logout, login
-from .forms import SignUpForm, ResponseForm
+from .forms import SignUpForm, ResponseForm, QuestionForm
 
 def login(request):
     """method for return login.html"""
@@ -21,12 +21,6 @@ def signup(request):
             user = form.save()
             user.refresh_from_db()  # load the profile instance created by the signal
             user.save()
-            """name = form.cleaned_data.get('name')
-            last_name = form.cleaned_data.get('last_name')
-            pseudo = form.cleaned_data.get('pseudo')
-            job = form.cleaned_data.get('job')
-            password = form.cleaned_data.get('password')"""
-            #login(request, Users)
             return redirect('blog/useraccount.html')
     else:
         form = SignUpForm()
@@ -40,7 +34,7 @@ def useraccount(request):
     liste.append(quest)
     return render(request, 'blog/useraccount.html', {'quest': liste})
 
-#def newquesttion():
+
 
 def questionResponse(request):
     """method for return questionResponse.html with some question and response in bdd """
@@ -57,91 +51,35 @@ def questionResponse(request):
 
 
 def response(request):
+    """method for post response"""
     if request.method == 'POST':
         form = ResponseForm(request.POST)
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()  # load the profile instance created by the signal
             user.save()
-            """name = form.cleaned_data.get('name')
-            last_name = form.cleaned_data.get('last_name')
-            pseudo = form.cleaned_data.get('pseudo')
-            job = form.cleaned_data.get('job')
-            password = form.cleaned_data.get('password')"""
-            #login(request, Users)
             return redirect('blog/questionResponse.html')
     else:
         form = ResponseForm()
     return render(request, 'blog/useraccount.html', {'form': form})
     
-    """"# if this is a POST request we need to process the form data
 
-        # create a form instance and populate it with data from the request:
-    form = response(request.Post or None)
-        # check whether it's valid:
-    if form.is_valid():
-        Response = form.save()
-        Response.refresh_from_db()
-        Response.save()
-        title =  form.cleaned_data.get('title')
-        content = form.cleaned_data.get('content')
-        publishing_date = form.cleaned_data.get('date')
-        updated_date = form.cleaned_data.get('update')
-        author = form.cleaned_data.get('author')
 
-        return  HttpResponseRedirect('/blog/questionResponse.html')
-        # if a GET (or any other method) we'll create a blank form
+def questions(request):
+    """method for post questions"""
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.save()
+            return redirect('blog/useraccount.html')
     else:
-        form = ResponseForm()
-
-        return render(request, 'questionResponse.html', {'form':form})"""
-
-
+        form = QuestionForm()
+    return render(request, 'blog/createaccount.html', {'form': form})
 
 
 def handler404(request):
     return render(request,'errors/404.html',status=404)
 
 
-"""class LoginView(SuccessURLAllowedHostsMixin, FormView):
-    form_class = AuthenticationForm
-    authentication_form = None
-    template_name = 'templates/login.html'"""
-
-
-"""def login(request):
-    if request.user.is_authenticated:
-        return render(request, 'home.html')
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            form = AuthenticationForm(request.POST)
-            return render(request, 'login.html', {'form': form})
-    else:
-        form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})"""
-
-
-"""def createaccount(request):
-    if request.method == 'POST':
-        form = Users(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = Users()
-    return render(request, 'blog/createaccount.html', {'form': form})"""
-
-
-"""def signout(request):
-    logout(request)
-    return redirect('login')"""
