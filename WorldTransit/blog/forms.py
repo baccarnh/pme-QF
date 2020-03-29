ae4f0a8514c80868f750ba503e2384ba09ef5793
nicos <nicolas.turck@gmail.com>
2020-03-29 10:32:20 +0200

    change method for dsiplay response and new question in questionResponse.html

--- WorldTransit/blog/forms.py
+++ WorldTransit/blog/forms.py
@@ -6,12 +6,14 @@ from .models import Users, Response, Questions
 
 
 class ResponseForm(forms.ModelForm):
+    # i use classe for extend the model for create a form based in this
     class Meta:
         model = Response
         fields = '__all__'
 
 
 class QuestionForm(forms.ModelForm):
+    # i use classe for extend the model for create a form based in this
     class Meta:
         model = Questions
         fields = '__all__'
--- WorldTransit/blog/views.py
+++ WorldTransit/blog/views.py
@@ -2,7 +2,7 @@ from django.forms import models
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Questions, Response, Users
 from django.contrib.auth import authenticate, login, logout
-from .forms import QuestionForm
+from .forms import QuestionForm, ResponseForm
 from django.urls import reverse
 import hashlib
 from datetime import timezone
@@ -55,41 +55,52 @@ def useraccount(request):
     return render(request, 'blog/useraccount.html', {'user': user})
 
 
-def questionResponse(request):
-    """method for return questionResponse.html with some question and response in bdd """
-    if request.method == 'GET':
-        quest = get_object_or_404(Questions, id=2)
-        liste = list()
-        liste.append(quest)
-
-        resp = Response.objects.filter(user_id=1)
-        liste2 = list()
-        liste2.append(resp)
-        return render(request, 'blog/questionResponse.html', {'quest': liste, 'resp': liste2})
-
-    return render(request, 'blog/questionResponse.html', {'user': user})
-
-
 def response(request):
-    """method for post response"""
-    if request.POST.get("reponse") is not None:
-        u = Questions(content=request.POST.get("reponse"))
-        u.save()
-        return render(request, 'blog/questionResponse.html', {'user': user})
-    return render(request, 'blog/questionReponse.html', {'user': user})
+    """method for display question and response """
+    global user
+    # i get the objezct in bdd and i add it in list
+    quest = get_object_or_404(Questions, id=2)
+    liste = list()
+    liste.append(quest)
+    # i get the objezct in bdd and i add it in list
+    resp = Response.objects.filter(user_id=1)
+    liste2 = list()
+    liste2.append(resp)
+    """i display form with django forms based in class response in models 
+        with class meta in forms.py"""
+    if request.method == 'POST':
+        form = ResponseForm(request.POST)
+        #if the form is ok
+        if form.is_valid():
+            resp = form.save()
+            resp.refresh_from_db()  # load the profile instance created by the signal
+            resp.save()
+            #i save it and i return to home page
+            return redirect('home')
+    else:# else a resent the pages with form
+        form = ResponseForm()
+        #i show the pages and i send the list and the form for dsiplay elems
+    return render(request, 'blog/questionResponse.html', {'quest': liste, 'resp': liste2, 'form': form})
 
+    def handler404(request):
+        return render(request, 'errors/404.html', status=404)
 
 def questions(request):
-    """method for post questions"""
+    """i display form with django forms based in class response in models
+           with class meta in forms.py"""
     if request.method == 'POST':
         form = QuestionForm(request.POST)
+        #if the form is ok
         if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.save()
+            quest = form.save()
+            quest.refresh_from_db()  # load the profile instance created by the signal
+            quest.save()
+            #i save it in the bdd
+            #and return to usercaccount page
             return redirect('useraccount.html')
     else:
         form = QuestionForm()
+        #if the form is not ok  display again the page and send form to display this in html
     return render(request, 'blog/newquestions.html', {'form': form})
 
     def handler404(request):
